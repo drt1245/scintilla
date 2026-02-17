db984f65ee992fdfdfc031ca4d6dc0333d64ec00
Damon Tarry <drt1245@gmail.com>
2026-02-17 15:57:22 -0800

    change default font to monospaced courier

--- fltk/PlatFLTK.cxx
+++ fltk/PlatFLTK.cxx
@@ -1058,7 +1058,7 @@ ColourRGBA Platform::ChromeHighlight()
 
 const char* Platform::DefaultFont()
 {
-	return Fl::get_font_name(FL_HELVETICA); //TODO: is FL_HELVETICA correct?
+	return Fl::get_font_name(FL_COURIER);//TODO: default fltk font is Helvetica, but for Scintilla, a monospaced font seems more appropriate
 }
 
 int Platform::DefaultFontSize()
